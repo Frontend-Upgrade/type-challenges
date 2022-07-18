2778904364359ad1e23609c1393bada127baff2f
Your Name <wjdgml3834@naver.com>
2022-07-19 01:28:29 +0900

    Hello World 문제 테스트 케이스 추가

--- KimJungHee/01.Hello_World.ts
+++ KimJungHee/01.Hello_World.ts
@@ -1,3 +1,7 @@
 //오류 수정 전 코드:  type HelloWorld = any
 
 type HelloWorld = string; // 수정 후 => 타입을 string으로 지정해주었다.
+
+import type { Equal, Expect, NotAny } from "../utils";
+
+type cases = [Expect<NotAny<HelloWorld>>, Expect<Equal<HelloWorld, string>>];
